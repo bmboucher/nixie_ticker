010e8ef34c9b43b8551b73e7cafc58a126484af1
Brian Boucher <bboucher@maxeler.com>
2018-08-30 15:04:41 -0500

    Fixing compilation error

--- software/src/nixie_driver.cpp
+++ software/src/nixie_driver.cpp
@@ -156,7 +156,7 @@ int main(void) {
 	while (true) {
 		std::string input;
 		std::cout << "> " << std::flush;
-		getline(std::cin, lines);
+		getline(std::cin, input);
 		std::cout << std::endl;
 		std::cout << "\tINPUT = " << input << std::endl;
 		display.write(input);
