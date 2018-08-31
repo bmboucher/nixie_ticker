e90d70ef30e8acbdb71f60ae7f6cb4823d316e63
Brian Boucher <bboucher@maxeler.com>
2018-08-31 09:40:56 -0500

    Moving executable sections to nixie_tester.cpp file

--- software/src/nixie_driver.cpp
+++ software/src/nixie_driver.cpp
@@ -2,8 +2,6 @@
 
 #include <pigpiod_if2.h>
 #include <iostream>
-#include <stdint.h>
-#include <vector>
 
 const uint32_t LATCH_PIN = 3;
 const uint32_t CLK_PIN   = 2;
@@ -140,44 +138,4 @@ void NixieDisplay::write(const std::string& str) {
 	}
 	set_anode(anode_flag);
 	update();
-}
-
-void test_single_register(NixieDisplay& display, uint8_t reg) {
-	bool curr_setting = display.get_register(reg);
-	std::cout << "\tFLIPPING REGISTER " << (int)reg << " (CURRENTLY " << curr_setting << ")" << std::endl;
-	display.set_register(reg, !curr_setting);
-	display.update();
-}
-
-void test_led_counter(NixieDisplay& display, uint32_t counter) {
-	for (uint8_t led_num = 0; led_num < NUM_NIXIES; led_num++) {
-		display.set_led(led_num, counter & 1);
-		counter = counter >> 1;
-	}
-	display.update();
-}
-
-
-int main(void) {
-	NixieDisplay display;
-	while (true) {
-		std::string input;
-		std::cout << "> " << std::flush;
-		getline(std::cin, input);
-		display.write(input);
-	}
-	/*
-		uint32_t counter = 1;
-		while (counter < 1000) {
-			test_led_counter(display, counter);
-			counter++;
-		}
-
-		int reg = 0;
-		while (true) {
-			std::cout << "Enter register: " << std::flush;
-			std::cin >> reg;
-			test_single_register(display, reg);
-		}
-	*/
 }
