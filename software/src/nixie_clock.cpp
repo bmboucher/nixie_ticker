ef3aa297e8eca5bc4dd94e59597176c4b047a564
Brian Boucher <bmboucher@gmail.com>
2018-09-01 13:40:57 -0400

    Adding cycling LEDs to nixie_clock

--- software/src/nixie_clock.cpp
+++ software/src/nixie_clock.cpp
@@ -1,17 +1,45 @@
 #include <nixie_driver.hpp>
 #include <time.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 void display_two_digit(int value, NixieDisplay& display, uint8_t nixie) {
 	display.set_nixie_digit(nixie,     value / 10);
 	display.set_nixie_digit(nixie + 1, value % 10);
 }
 
+long long timeMillis() {
+	struct timeval tp;
+	gettimeofday(&tp, NULL);
+	return (long long)tp.tv_sec * 1000L + tp.tv_usec / 1000;
+}
+
+const long long DP_DELAY = 500;
+const long long LED_DELAY = 200;
+
 int main() {
 	NixieDisplay display;
 	display.set_anode(true);
 	bool dp = true;
+	long long last_dp = timeMillis();
+	long long last_led = timeMillis();
+	uint8_t led_num = 0;
+	bool led_fwd = true;
+	display.set_led(led_num, true);
 	while(true) {
+		long long curr_time = timeMillis();
+		if (curr_time - last_dp >= DP_DELAY) {
+			dp = !dp;
+			last_dp = curr_time;
+		}
+		if (curr_time - last_led >= LED_DELAY) {
+			display.set_led(led_num, false);
+			if (led_num == NUM_NIXIES - 1 && led_fwd) led_fwd = false;
+			if (led_num == 0 && !led_fwd) led_fwd = true;
+			led_num = led_fwd ? led_num + 1 : led_num - 1;
+			display.set_led(led_num, true);
+		}
+
 		time_t rawtime = time(NULL);
 		struct tm* timeinfo = localtime(&rawtime);
 		display_two_digit(timeinfo->tm_hour, display, 0);
@@ -20,7 +48,5 @@ int main() {
 		display.set_decimal_point(1, dp);
 		display.set_decimal_point(3, dp);
 		display.update();
-		usleep(100000);
-		dp = !dp;
 	}
 }
