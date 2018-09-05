4f394608f2dda085275cb167868f1298c7f149c0
Brian Boucher <bboucher@maxeler.com>
2018-09-05 07:55:33 -0500

    Cleaning up nixie_ticker.cpp code, pointing to IEX

--- software/src/nixie_ticker.cpp
+++ software/src/nixie_ticker.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <nixie_driver.hpp>
+#include <unistd.h>
 
 int main(void)
 {
@@ -9,43 +10,20 @@ int main(void)
 	
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	curl = curl_easy_init();
-	  if(curl) {
-		      curl_easy_setopt(curl, CURLOPT_URL, "https://example.com/");
-		       
-#ifdef SKIP_PEER_VERIFICATION
-		          /*
-			   *      * If you want to connect to a site who isn't using a certificate that is
-			   *           * signed by one of the certs in the CA bundle you have, you can skip the
-			   *                * verification of the server's certificate. This makes the connection
-			   *                     * A LOT LESS SECURE.
-			   *                          *
-			   *                               * If you have a CA cert for the server stored someplace else than in the
-			   *                                    * default bundle, then the CURLOPT_CAPATH option might come handy for
-			   *                                         * you.
-			   *                                              */ 
-		          curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-#endif
-			   
-#ifdef SKIP_HOSTNAME_VERIFICATION
-			      /*
-			       *      * If the site you're connecting to uses a different host name that what
-			       *           * they have mentioned in their server certificate's commonName (or
-			       *                * subjectAltName) fields, libcurl will refuse to connect. You can skip
-			       *                     * this check, but this will make the connection less secure.
-			       *                          */ 
-			      curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-#endif
-			       
-			          /* Perform the request, res will get the return code */ 
-			          res = curl_easy_perform(curl);
-				      /* Check for errors */ 
-				      if(res != CURLE_OK)
-					            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-								                  curl_easy_strerror(res));
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "https://api.iextrading.com/1.0/stock/aapl/price");
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+		/* Perform the request, res will get the return code */ 
+		res = curl_easy_perform(curl);
+		/* Check for errors */ 
+		if (res != CURLE_OK)
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+							curl_easy_strerror(res));
 				       
-				          /* always cleanup */ 
-				          curl_easy_cleanup(curl);
-					    }
+		/* always cleanup */ 
+		curl_easy_cleanup(curl);
+	}
 	   
 	curl_global_cleanup();
 	return 0;
