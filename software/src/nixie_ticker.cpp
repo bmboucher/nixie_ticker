cb76c0358e2c2e305bc42492c8ebb997f1747ad7
Brian Boucher <bboucher@maxeler.com>
2018-09-05 08:07:54 -0500

    Adding custom write_data

--- software/src/nixie_ticker.cpp
+++ software/src/nixie_ticker.cpp
@@ -2,9 +2,20 @@
 #include <curl/curl.h>
 #include <nixie_driver.hpp>
 #include <unistd.h>
+#include <string>
+#include <iostream>
+
+size_t write_data(void* buffer, size_t size, size_t nmembd, void* userp) {
+	NixieDisplay* display = (NixieDisplay*)userp;
+	std::string price_str = std::string("X", 7 - size) + std::string((const char*)buffer);
+	display.write(price_str);
+	std::cout << price_str;
+}
 
 int main(void)
 {
+	NixieDisplay display;
+	
 	CURL *curl;
 	CURLcode res;
 	
@@ -14,9 +25,11 @@ int main(void)
 		curl_easy_setopt(curl, CURLOPT_URL, "https://api.iextrading.com/1.0/stock/aapl/price");
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
 		/* Perform the request, res will get the return code */ 
 		for (int i = 0; i < 10; i++) {
 			res = curl_easy_perform(curl);
+			usleep(250000);
 		}
 		/* Check for errors */ 
 		if (res != CURLE_OK)
